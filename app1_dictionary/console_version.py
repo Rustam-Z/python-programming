dfdb12b52456f386774694845fdf93ed0c13df68
Rustam-Z <zokirovrustam202@gmail.com>
2020-08-12 19:06:04 +0500

    Tk version done

--- app1_dictionary/console_version.py
+++ app1_dictionary/console_version.py
@@ -3,6 +3,7 @@ from difflib import get_close_matches
 
 data = json.load(open("app1_dictionary/data.json"))
 
+
 def translate(w):
     w = w.lower()
 
@@ -10,18 +11,24 @@ def translate(w):
         return data[w]
     elif w.title() in data:
         return data[w.title()]
-    elif w.upper() in data: #in case user enters words like USA or NATO
+    elif w.upper() in data:
+        # in case user enters words like USA or NATO
         return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y if yes, "
+                   "or N if no: "
+                   % get_close_matches(w, data.keys())[0])
         if yn == "Y" or yn == "y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N" or yn == "n":
-            return "The word doesn't exist. Please double check it."
+            return "The word doesn't exist. " \
+                   "Please double check it."
         else:
             return "We didn't understand your entry."
     else:
-        return "The word doesn't exist. Please double check it."
+        return "The word doesn't exist. " \
+               "Please double check it."
+
 
 word = input("Enter word: ")
 output = translate(word)
--- app1_dictionary/tk_gui_version.py
+++ app1_dictionary/tk_gui_version.py
@@ -20,6 +20,7 @@ data = json.load(open("app1_dictionary/data.json"))
 BOOKMARKS = []
 HISTORY = []
 
+
 class MenuBar(tk.Menu):
     def __init__(self, master):
         tk.Menu.__init__(self, master)
@@ -94,7 +95,7 @@ class MenuBar(tk.Menu):
 
         self.e1 = ttk.Entry(self.frame1)
         self.e1.grid(row=0, column=1, padx=10, pady=10)
-    
+
         self.l2 = ttk.Label(self.frame1, text="", wraplength=250)
         self.l2.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
@@ -105,39 +106,39 @@ class MenuBar(tk.Menu):
         # Add a scrollbar
         scrollbar = ttk.Scrollbar(self.frame2)
         scrollbar.pack(side=RIGHT, fill=Y)
-        
-        self.list_box = tk.Listbox(self.frame2, yscrollcommand=scrollbar.set, width=50, height=50, borderwidth=0, highlightthickness=0, selectmode=SINGLE)
+
+        self.list_box = tk.Listbox(self.frame2, yscrollcommand=scrollbar.set, width=50, height=50, borderwidth=0,
+                                   highlightthickness=0, selectmode=SINGLE)
         self.list_box.pack(padx=10, pady=10, side=TOP, anchor=NW)
-        
+
         self.list_box.bind("<<ListboxSelect>>", self.from_history)
         scrollbar.config(command=self.list_box.yview)
-    
 
     def translate(self):
         w = self.e1.get()
         w = w.lower()
-    
+
         if w in data:
             self.l2['text'] = data[w]
-            self.list_box.insert(END, w) # History
+            self.list_box.insert(END, w)  # History
             HISTORY.append(w)
 
         elif w.title() in data:
-            self.l2['text'] = data[w.title()] 
-            self.list_box.insert(END, w.title()) # History
+            self.l2['text'] = data[w.title()]
+            self.list_box.insert(END, w.title())  # History
             HISTORY.append(w.title())
 
 
-        elif w.upper() in data: # in case user enters words like USA or NATO
+        elif w.upper() in data:  # in case user enters words like USA or NATO
             self.l2['text'] = data[w.upper()]
-            self.list_box.insert(END, w.upper()) # History
+            self.list_box.insert(END, w.upper())  # History
             HISTORY.append(w.upper())
 
         elif len(get_close_matches(w, data.keys())) > 0:
             yn = messagebox.askquestion("Hint", "Did you mean '%s' instead?" % get_close_matches(w, data.keys())[0])
             if yn == "yes":
                 self.l2['text'] = data[get_close_matches(w, data.keys())[0]]
-                self.list_box.insert(END, get_close_matches(w, data.keys())[0]) # History
+                self.list_box.insert(END, get_close_matches(w, data.keys())[0])  # History
                 HISTORY.append(get_close_matches(w, data.keys())[0])
 
             elif yn == "no":
@@ -162,7 +163,6 @@ class MenuBar(tk.Menu):
         # delete the last word
         # self.e1.delete(0, END)
 
-
     # For translate the words from History
     def from_history(self, *args):
         for i in self.list_box.curselection():
@@ -171,25 +171,24 @@ class MenuBar(tk.Menu):
             if HISTORY[i] in data:
                 messagebox.showinfo("{}".format(HISTORY[i]), data[HISTORY[i]])
 
-
     def bookmarks(self):
         self.hide_all_frames()
         self.bookmarks_frame.pack(fill="both", expand=1)
-        p2 = ttk.Label(self.bookmarks_frame, text="Here you can your boookmarks")
+        p2 = ttk.Label(self.bookmarks_frame, text="Here you can find your bookmarks")
         p2.pack(padx=10, pady=10)
 
         # Add a scrollbar
         scrollbar = ttk.Scrollbar(self.bookmarks_frame)
         scrollbar.pack(side=RIGHT, fill=Y)
-        
-        self.list_box_1 = tk.Listbox(self.bookmarks_frame, yscrollcommand=scrollbar.set, width=50, height=50, borderwidth=0, highlightthickness=0, selectmode=SINGLE)
+
+        self.list_box_1 = tk.Listbox(self.bookmarks_frame, yscrollcommand=scrollbar.set, width=50, height=50,
+                                     borderwidth=0, highlightthickness=0, selectmode=SINGLE)
         self.list_box_1.pack(padx=10, pady=10, side=TOP, anchor=NW)
 
         self.list_box_1.insert('end', *BOOKMARKS)
-        
+
         self.list_box_1.bind("<<ListboxSelect>>", self.from_boormarks)
         scrollbar.config(command=self.list_box_1.yview)
-    
 
     def boookmark_it(self):
         if self.var.get() == 1:
@@ -205,7 +204,6 @@ class MenuBar(tk.Menu):
 
             if BOOKMARKS[i] in data:
                 messagebox.showinfo("{}".format(BOOKMARKS[i]), data[BOOKMARKS[i]])
-  
 
     def info_about(self):
 
@@ -216,14 +214,12 @@ class MenuBar(tk.Menu):
         self.info_about_frame.pack(fill="both", expand=1)
         ttk.Label(self.info_about_frame, text="ABOUT", background='#c5e3e0').pack()
         ttk.Label(self.info_about_frame, text="EightSoft Academy | Rustam-Z | Alimov-8").pack()
-        
+
         link1 = tk.Label(self.info_about_frame, text="Source Code", fg='#215fdb')
         link1.pack()
-        link1.bind("<Button-1>", lambda e: callback("https://github.com/Rustam-Z/python-projects-eightsoft/tree/master/App1_Dictionary"))
+        link1.bind("<Button-1>", lambda e: callback(
+            "https://github.com/Rustam-Z/learning-area/tree/master/app1_dictionary"))
 
-    
-
- 
 
 class App(tk.Tk):
     def __init__(self, master):
@@ -235,7 +231,6 @@ class App(tk.Tk):
 
 
 if __name__ == "__main__":
-
     # Create object
     app = App(None)
     app.title("My Dictionary App")
